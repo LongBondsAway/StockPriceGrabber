d494d14fb39bb538094ba25b48ea931c197dd21b
Brett Dommer <82011375+LongBondsAway@users.noreply.github.com>
2021-04-06 19:29:37 -0700

    Add files via upload

---
+++ Stock_Price_Tool.py
@@ -0,0 +1,18 @@
+#Stock Price Grabber
+
+#import required tools/packages
+import yfinance as yf
+
+#define the ticker symbol
+tickerSymbol = 'AAPL'
+
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2021-1-1', end='2021-4-5')
+
+#see your data
+tickerDf
+
+print(tickerDf)
